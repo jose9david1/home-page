aa64ef260093e098052057aa2ceb590d6f4c0f77
jose9david1 <jsrosas02@gmail.com>
2023-08-26 16:50:23 -0300

    componentes-clase

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import '../hoja-de-estilo/Testimonio.css'
 
-function Testimonio (props) {
-	return (
-		<div className='contenedor-testimonio'>
-			<img
-				className='imagen-testimonio'
-				src={require(`../imagenes/${props.imagen}.png`)}
-				alt='Foto de Emma'/>
-			<div className='contenedor-texto-testimonio'>
-				<p className='nomre-testimonio'>
-					<strong>{props.nombre}</strong> en {props.pais}</p>
-				<p className='cargo-testimonio'>
-					<strong>{props.cargo} </strong> en {props.empresa} </p>
-				<p className='texto-testimonio'>"{props.testimonio}"</p>
-			</div>
-		</div>
-	)
 
+class Testimonio extends React.Component {
+	render() {
+		return (
+			<div className='contenedor-testimonio'>
+				<img
+					className='imagen-testimonio'
+					src={require(`../imagenes/${this.props.imagen}.png`)}
+					alt={`Foto de ${this.props.nombre}`} />
+				<div className='contenedor-texto-testimonio'>
+					<p className='nomre-testimonio'>
+						<strong>{this.props.nombre}</strong> en {this.props.pais}</p>
+					<p className='cargo-testimonio'>
+						<strong>{this.props.cargo} </strong> en {this.props.empresa} </p>
+					<p className='texto-testimonio'>"{this.props.testimonio}"</p>
+				</div>
+			</div>
+		)
+
+	}
 }
+
 
 export default Testimonio;
 
